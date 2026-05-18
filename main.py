560391b0f298c786cd1514dee5e52a6451c54443
mariam <mariam.zaki502@gmil.com>
2026-05-18 16:54:35 +0300

    update base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from routes import base
+from routers import base
 
 app = FastAPI()
 
--- routers/base.py
+++ routers/base.py
@@ -8,10 +8,7 @@ base_router = APIRouter(
 
 @base_router.get("/")
 async def welcome():
-    app_name = os.getenv('APP_NAME')
-    app_version = os.getenv('APP_VERSION')
-
     return {
-        "app_name": app_name,
-        "app_version": app_version,
+
+    "massage":"hi"
     }
