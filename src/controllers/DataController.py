214788768a11a815be8e131a2b79650f5a75c856
mariam <mariam.zaki502@gmil.com>
2026-05-19 15:13:18 +0300

    make validation for uploaded_file

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,5 +1,19 @@
 from .BaseController import BaseController
+from fastapi import UploadFile
+from models import ResponseSignal
+import re
+import os
 
 class DataController(BaseController):
     def __init__(self):
         super().__init__
+
+    def validate_uploaded_file(self, file: UploadFile):
+
+        if file.content_type not in self.app_settings.FILE_ALLOWED_TYPES:
+            return False, ResponseSignal.FILE_TYPE_NOT_SUPPORTED.value
+
+        if file.size > self.app_settings.FILE_MAX_SIZE * self.size_scale:
+            return False, ResponseSignal.FILE_SIZE_EXCEEDED.value
+
+        return True, ResponseSignal.FILE_VALIDATED_SUCCESS.value
--- src/routers/data.py
+++ src/routers/data.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
 import os
 from helpers.config import get_settings, Settings
+from controllers import DataController, ProjectController
+from fastapi.responses import JSONResponse
 
 
 data_router = APIRouter(
@@ -12,4 +14,15 @@ data_router = APIRouter(
 async def upload_data(project_id: str, file: UploadFile,
                       app_settings: Settings = Depends(get_settings)):
         
-    pass
+    # validate the file properties
+    data_controller = DataController()
+
+    is_valid, result_signal = data_controller.validate_uploaded_file(file=file)
+
+    if not is_valid:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": result_signal
+            }
+        )
