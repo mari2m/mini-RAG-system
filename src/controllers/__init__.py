cda1446cf2766ae916b07c286212062c25ff5aa1
mariam <mariam.zaki502@gmil.com>
2026-05-20 01:46:13 +0300

    update init

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,3 +1,3 @@
-from .BaseController import BaseController
 from .DataController import DataController
 from .ProjectController import ProjectController
+from .ProcessController import ProcessController
