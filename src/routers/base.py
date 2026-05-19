44cc4c89987d3b90db901ec04ed9a026f578af86
mariam <mariam.zaki502@gmil.com>
2026-05-19 14:22:31 +0300

    add files config

--- src/routers/base.py
+++ src/routers/base.py
@@ -9,7 +9,7 @@ base_router = APIRouter(
 )
 
 @base_router.get("/")
-async def welcome(app_settings=Depends(get_settings)):
+async def welcome(app_settings: Settings = Depends(get_settings)):
     #app_settings=get_settings()
     app_name=app_settings.APP_NAME
     app_version=app_settings.APP_VERSION
